d8092c90134a07018bd010300bc5673bb890ae2b
Jake Young <jake@acpdecisions.org>
2016-12-28 16:13:08 -0500

    Initial Commit

---
+++ AgreementKit/AgreementKit.h
@@ -0,0 +1,19 @@
+//
+//  AgreementKit.h
+//  AgreementKit
+//
+//  Created by Jake Young on 12/28/16.
+//  Copyright © 2016 Jake Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AgreementKit.
+FOUNDATION_EXPORT double AgreementKitVersionNumber;
+
+//! Project version string for AgreementKit.
+FOUNDATION_EXPORT const unsigned char AgreementKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AgreementKit/PublicHeader.h>
+
+
